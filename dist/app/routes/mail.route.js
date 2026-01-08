8016af4b3e3055532948b2e33719cbf8c0537fe0
developer-shourav <developer.shourav1@gmail.com>
2026-01-09 01:30:36 +0600

    Backend Primary Optimization completed

--- dist/app/routes/mail.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const mail_controller_1 = require("../controllers/mail.controller");
-const router = (0, express_1.Router)();
-// Public endpoint
-router.post("/send", mail_controller_1.sendMailController);
-exports.default = router;
